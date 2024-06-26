e67209a2556004e17157da5fd193d3d1805a5988
Sammy Tabally <sammytabally@hotmail.com>
2024-06-26 16:15:53 -0500

    New update for my project

--- src/components/NewsCardList/NewsCardList.jsx
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -6,7 +6,7 @@ import { hasSearchedContext } from "../../contexts/hasSearchedContext";
 const NewsCardList = ({}) => {
   const [cardsDisplayed, setCardsDisplayed] = useState(3);
 
-  const { searchResults } = useContext(searchResultContext);
+  const { searchResult } = useContext(searchResultContext);
 
   const { hasSearched } = useContext(hasSearchedContext);
 
@@ -14,22 +14,22 @@ const NewsCardList = ({}) => {
     setCardsDisplayed(cardsDisplayed + 3);
   };
 
-  console.log("search Results context", searchResults);
+  console.log("search Results context", searchResult);
   console.log("has Searched context", hasSearched);
-
+  
   return (
     <section className="news__card-section">
       {hasSearched ? (
         <>
           <h2 className="news__cards-title">News Articles</h2>
           <div className="news__cards-container">
-            {searchResults.slice(0, cardsDisplayed).map((result) => {
+            {searchResult.slice(0, cardsDisplayed).map((result) => {
                <NewsCard newsData={result} key={result.url} />
             })}
           </div>
           <button
             className={`newscards__button ${
-              cardsDisplayed >= searchResults.length
+              cardsDisplayed >= searchResult.length
                 ? "newscards__button_hidden"
                 : ""
             }`}
