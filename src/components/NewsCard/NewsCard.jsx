cb31c94daea6a19f1c011816eb80a58b417a2985
Sammy Tabally <sammytabally@hotmail.com>
2024-06-27 22:35:37 -0500

    New update for my project

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -3,51 +3,57 @@ import { useLocation } from "react-router-dom";
 import { keyWordContext } from "../../contexts/keyWordContext";
 import { currentPageContext } from "../../contexts/currentPageContext";
 import { useContext, useEffect } from "react";
+debugger
+function NewsCard({ newsData }) {
+  console.log("Rendering NewsCard", newsData);
 
+  let formattedDate;
 
-
-function NewsCard ({ newsData }) {
-
-    console.log("Rendering NewsCard", newsData);
-
-    let formattedDate;
-
-    if (newsData.publishedAt) {
-        formattedDate = new Date(newsData.publishedAt).toLocaleDateString("default", {
-            day: "numeric",
-            month: "long",
-            year: "numeric",
-        });
-    } else {
-        formattedDate = "";
-    }
-
-    const location = useLocation();
-
-    const { keyWord } = useContext(keyWordContext);
-
-    const { setCurrentPage } = useContext(currentPageContext);
-
-    useEffect(() => {
-        setCurrentPage(location.pathname);
-    }, [location.pathname, setCurrentPage]);
-
-    return (
-        <div className="news-card">
-            <a className="news-card__link" href={newsData.url} target="_blank" rel="noreferrer">
-                <img
-                    className="news-card__image"
-                    src={newsData.urlToImage}
-                    alt={newsData.title}
-                />
-                <div className="news-card__description">
-                    <h3 className="news-card__title">{newsData.title}</h3>
-                    <p className="news-card__date">{formattedDate}</p> 
-                    <p className="news-card__Keyword">{keyWord}</p> 
-                </div>  
-            </a>
-        </div>
+  if (newsData.publishedAt) {
+    formattedDate = new Date(newsData.publishedAt).toLocaleDateString(
+      "default",
+      {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      }
     );
+  } else {
+    formattedDate = "";
+  }
+
+  const location = useLocation();
+
+  const { keyWord } = useContext(keyWordContext);
+
+  const { setCurrentPage } = useContext(currentPageContext);
+
+  useEffect(() => {
+    setCurrentPage(location.pathname);
+  }, [location.pathname, setCurrentPage]);
+
+  return (
+    <div className="news-card">
+      <a
+        className="news-card__link"
+        href={newsData.url}
+        target="_blank"
+        rel="noreferrer">
+        {newsData.urlToImage && (
+          <img
+            className="news-card__image"
+            src={newsData.urlToImage}
+            alt={newsData.title}
+          />
+        )}
+        <div className="news-card__description">
+          <h3 className="news-card__title">{newsData.title}</h3>
+          <p className="news-card__date">{formattedDate}</p>
+          <p className="news-card__Keyword">{keyWord}</p>
+        </div>
+      </a>
+    </div>
+  );
 }
 
 export default NewsCard;
