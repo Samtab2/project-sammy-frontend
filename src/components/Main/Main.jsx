a5dade5319a2110deefab8508440ba6746776568
Sammy Tabally <sammytabally@hotmail.com>
2024-06-30 21:07:04 -0500

    New update for my project

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,17 @@
-import "./Main.css";
-import About from "../About/About";
-import NewsCardList from "../NewsCardList/NewsCardList";
-import SearchForm from "../SearchForm/SearchForm";
-import Preloader from "../Preloader/Preloader";
-import NotFound from "../NotFound/NotFound";
-import { useContext } from "react";
-import { searchResultContext } from "../../contexts/searchResultContext";
-import { hasSearchedContext } from "../../contexts/hasSearchedContext";
+import './Main.css';
+import About from '../About/About';
+import NewsCardList from '../NewsCardList/NewsCardList';
+import SearchForm from '../SearchForm/SearchForm';
+import Preloader from '../Preloader/Preloader';
+import NotFound from '../NotFound/NotFound';
+import { useContext } from 'react';
+import { searchResultContext } from '../../contexts/searchResultContext';
+import { hasSearchedContext } from '../../contexts/hasSearchedContext';
 
 function Main({ searchError, handleSearch, isLoading }) {
   const { searchResult } = useContext(searchResultContext);
   const { hasSearched } = useContext(hasSearchedContext);
+
 
   return (
     <main className="main">
@@ -28,7 +29,7 @@ function Main({ searchError, handleSearch, isLoading }) {
             connection issue or the server may be down. Please try again later.
           </p>
         ) : (
-          ""
+          ''
         )}
       </div>
       <About />
