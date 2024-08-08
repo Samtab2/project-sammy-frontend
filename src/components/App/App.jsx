03cd2d8398633364e1d1c1882a1d78e60ac6cd61
Sammy Tabally <sammytabally@gmail.com>
2024-08-07 20:27:43 -0500

    Update on my project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -112,6 +112,7 @@ function App() {
     checkToken()
       .then((res) => {
         if (res) {
+          setIsLoggedIn(true);
           setCurrentUser(res.data);
           getSavedArticles()
             .then((res) => {
