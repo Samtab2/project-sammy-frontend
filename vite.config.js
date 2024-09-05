1da776269d7bf679a4ccd8b75022d9c67d42fa3c
Sammy Tabally <sammytabally@gmail.com>
2024-09-04 20:53:38 -0500

    Update on my Project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "./Sammy_news_explorer-frontend/",
   plugins: [react()],
   server: { port: 3000 },
 });
